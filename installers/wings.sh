d3ed497be5ae575b49bca042a411f29eb766f83f
Whispers88 <68432130+Whispers88@users.noreply.github.com>
2024-12-11 16:31:11 +0800

    Add files via upload
    seems to have an issue with the sed command under the rocky | almalinux case. The -ne option combination is incorrect for sed

--- installers/wings.sh
+++ installers/wings.sh
@@ -183,7 +183,7 @@ configure_mysql() {
       sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
       ;;
     rocky | almalinux)
-      sed -ne 's/^#bind-address=0.0.0.0$/bind-address=0.0.0.0/' /etc/my.cnf.d/mariadb-server.cnf
+      sed -i 's/^#bind-address=.*$/bind-address=0.0.0.0/' /etc/my.cnf.d/mariadb-server.cnf
       ;;
     esac
 
@@ -192,6 +192,7 @@ configure_mysql() {
 
   success "MySQL configured!"
 }
+
 
 # --------------- Main functions --------------- #
 
